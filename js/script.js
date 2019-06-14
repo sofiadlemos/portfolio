46a25bbacb0a1240289581733c6093093d1871d4
sofiadlemos <sofiaduquelemos@ua.pt>
2019-06-14 23:31:24 +0100

    Experiências Trabalho

--- js/script.js
+++ js/script.js
@@ -3,4 +3,28 @@ $(function () {
 		var $nav = $(".navbar-fixed-top");
 		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
+
+
+	var today = new Date();
+	var hourNow = today.getHours();
+	var greeting = "Bem-vindo";
+	var bio = window.document.getElementById("welcome").textContent;
+
+	if (hourNow > 18) {
+		greeting = 'Boa noite';
+	} else if (hourNow > 12) {
+		if (hourNow > 12) {
+			greeting = 'Boa tarde';
+		} else if (hourNow > 0) {
+			greeting = 'Bom dia';
+		} else {
+			greeting = 'Bem-vindo';
+		}
+
+	}
+
+	window.document.getElementById("welcome").innerHTML = greeting + ", sou a Sofia!<br/>"+bio;
+
 });
+
+
